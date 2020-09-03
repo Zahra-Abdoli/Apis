e571a6b2f715e1dfaa0fe1475f6fa7e2cfd4e22a
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-09-03 15:37:51 +0200

    LAb 7 not finished

---
+++ Lab7/Entities/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ent
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab7.Entities
+{
+  
+        public class DataContext : DbContext
+        {
+            public DataContext(DbContextOptions<DataContext> option) : base(option)
+            {
+
+            }
+
+            //Blir tabell i DB
+            public DbSet<User> Users { get; set; }
+        }
+    }
+
